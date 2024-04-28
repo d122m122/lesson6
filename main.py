9f714cae2461267482aca16c238e382ad7f29d8a
Doniyor <domanick191201@gmail.com>
2024-04-28 11:32:26 +0500

    masala

---
+++ main.py
@@ -0,0 +1,31 @@
+#TASK1
+# def get_first_value(lst):
+#     return lst[0]
+#
+# print(get_first_value([1, 2, 3]))
+# print(get_first_value([80, 5, 100]))
+# print(get_first_value([-500, 0, 50]))
+
+
+#TASK2
+# def concat(list, list2):
+#     natija = list + list2
+#     return natija
+# print(concat([1, 3, 5], [2, 6, 8]))
+# print(concat([7, 8], [10, 9, 1, 1, 2]))
+# print(concat([4, 5, 1], [3, 3, 3, 3, 3]))
+
+#TASK3
+# def get_last_item(list):
+#     return list[-1]
+# print(get_last_item([1, 2, 3,]))
+# print(get_last_item(["cat", "dog", "duck"]))
+# print(get_last_item([True, False, True]))
+# print(get_last_item([7, "String", False]))
+
+#TASK4
+# def make_pair(num, num1):
+#     return num, num1
+# print(make_pair(1, 2))
+# print(make_pair(51, 21))
+# print(make_pair(512124, 215))
